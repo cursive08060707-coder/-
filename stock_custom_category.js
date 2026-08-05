b2de690cb7e3adfb2624c4a6ebc19366a290dcdd
Cursive <cursive08060707@gmail.com>
2026-08-05 19:20:30 +0800

    更新 stock_custom_category.js

---
+++ stock_custom_category.js
@@ -0,0 +1,2 @@
+// 由 notes_server.py 与 stock_custom_category.json 同步生成。
+window.BOGE_CATEGORY_DATA={"version":1,"dates":{"2026-08-05":{"半导体":{"300686":"玻璃基板","603078":"电子化学品","603823":"电子化学品","002222":"玻璃基板"}}},"updatedAt":"2026-08-05T11:17:11.909Z"};
